6097254faab7b6e620c56ad44a69a40f2b0885a2
frankline <franklineraphael45@gmail.com>
2025-09-25 23:00:30 +0100

    push

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { Section } from "lucide-react";
+import React from "react";
+import HeaderBox from "../components/HeaderBox";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const loggedIn = { firstName: "Frankline" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Acess and manage your account and transactions efficiently."
+          />
+          Welcome, Frankline
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
